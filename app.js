c38dc4d627b17ce5a196b4788c671cb1ebc71361
Marge13333 <113912686+Marge13333@users.noreply.github.com>
2023-04-21 00:09:57 +0400

    create js

---
+++ app.js
@@ -0,0 +1,73 @@
+//////task 1
+
+function sum(number = 10) {
+    return number * number;
+}
+console.log(sum())
+
+////// task 2
+function price(priceNumber= 1000, percent= 10) {
+    return priceNumber - (priceNumber * (percent/100))
+};
+console.log(price())
+
+/////// task 3
+function getRandomInt(max=100) {
+    return Math.floor(Math.random(0,100)*max);
+  }
+  console.log(getRandomInt())
+
+ ///////task 4
+ function findRate(number=123456789) {
+    ??????
+ }
+/////////task 5
+const currencies = [
+	{
+		code: "AED",
+		quantity: 10,
+		rateFormated: "6.8886",
+		diffFormated: "0.0082",
+		rate: 6.8886,
+		name: "არაბეთის გაერთიანებული საამიროების დირჰამი",
+		diff: -0.0082,
+		date: "2023-04-10T17:45:04.522Z",
+		validFromDate: "2023-04-11T00:00:00.000Z",
+	},
+	{
+		code: "AMD",
+		quantity: 1000,
+		rateFormated: "6.4869",
+		diffFormated: "0.0334",
+		rate: 6.4869,
+		name: "სომხური დრამი",
+		diff: -0.0334,
+		date: "2023-04-10T17:45:04.522Z",
+		validFromDate: "2023-04-11T00:00:00.000Z",
+	},
+	{
+		code: "CZK",
+		quantity: 10,
+		rateFormated: "1.1820",
+		diffFormated: "0.0010",
+		rate: 1.182,
+		name: "ჩეხური კრონა",
+		diff: 0.001,
+		date: "2023-04-10T17:45:04.522Z",
+		validFromDate: "2023-04-11T00:00:00.000Z",
+	},
+	{
+		code: "EUR",
+		quantity: 1,
+		rateFormated: "2.7599",
+		diffFormated: "0.0056",
+		rate: 2.7599,
+		name: "ევრო",
+		diff: -0.0056,
+		date: "2023-04-10T17:45:04.522Z",
+		validFromDate: "2023-04-11T00:00:00.000Z",
+	},
+];
+function currenciesMinRate (currencies) {
+    ????
+}
